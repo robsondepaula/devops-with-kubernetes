33d3aaa177b0adc636c0d5803193cf0fab5f37ee
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-09-20 15:35:52 -0300

    WIP: overengineered counter.

--- part2/2_07/pingpong/README.md
+++ part2/2_07/pingpong/README.md
@@ -1,3 +1,16 @@
+# Testing
+Spin up a local Postgres database:
+```
+docker-compose up -d
+```
+Start the service:
+```
+node index.js
+```
+Make a request:
+```
+curl http://localhost:3000/
+```
 # Building the image
 ```
 docker build . -t robsondepaula/pingpong:2_07
--- part2/2_07/pingpong/index.js
+++ part2/2_07/pingpong/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json(`Ping / Pongs: ${counter++}`);
+const { Pingpong } = require("./models");
+
+app.get("/", async (request, response) => {
+  const pingPong = await Pingpong.findOne();
+
+  let counter = pingPong.count + 1;
+
+  await Pingpong.update({ count: counter }, { where: { id: pingPong.id } });
+
+  response.json(`Ping / Pongs: ${counter}`);
 });
 
-let counter = 0;
 const PORT = process.env.SVC_PORT;
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT}`);
