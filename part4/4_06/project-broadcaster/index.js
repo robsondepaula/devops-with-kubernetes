5ec3180889d0c9dc319b65bb3bb00a7eb5faf8c2
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-11-18 14:31:35 -0300

    Fix incident.

--- part4/4_06/project-broadcaster/index.js
+++ part4/4_06/project-broadcaster/index.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 const { connect, JSONCodec } = require("nats");
 const axios = require("axios");
 
-const DISCORD_URL =
-  "https://discord.com/api/webhooks/910901658744160277/RzQGjuW5_K5TCF_9ijwlPFI5qhaA4p4NOxg1ZPo7Wj0uO3JxU3h2_DxKSGEFBcwRoBVP";
-
 let natsConnection;
 
 const init = async () => {
@@ -24,7 +21,7 @@ const init = async () => {
           console.log(todo);
           const botMessage = `I am broadcasting a ${todo.operation} operation with content ${todo.content} and status done=${todo.done}.`;
           
-          axios.post(DISCORD_URL, { content: botMessage });
+          axios.post(process.env.DISCORD_URL, { content: botMessage });
         }
       },
     });
