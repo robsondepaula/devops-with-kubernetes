5093d83649e95bd2bf6b581146d792299177bb55
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-11-27 09:09:35 -0300

    Initial working version of the dummy-site cloner.

--- part5/5_01/dummy-site/README.md
+++ part5/5_01/dummy-site/README.md
@@ -3,10 +3,12 @@ This is a web scrapper which creates a copy of a given website.
 
 The objective here is to validate the custom resource definition, DummySite, created to illustrate how to extend Kubernetes.
 
+It makes use of the [goclone](https://github.com/imthaghost/goclone) package to speed up the results.
+
 ## Local testing
 Execute:
 ```
-WEBSITE_URL=https://example.com/ bash -c 'go run .'
+GOPATH=~/go WEBSITE_URL=https://example.com/ bash -c 'go run .'
 ```
 
 ## Docker image
@@ -17,4 +19,9 @@ docker build . -t robsondepaula/dummy-site:5_01
 To publish:
 ```
 docker push robsondepaula/dummy-site:5_01
+```
+
+### Simple Dockerfile debugging
+```
+DOCKER_BUILDKIT=0 docker build . -t robsondepaula/dummy-site:5_01
 ```
--- part5/5_01/dummy-site/main.go
+++ part5/5_01/dummy-site/main.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
+	"os/exec"
 )
+
+func GetFolderName(websiteUrl string) (string, error) {
+
+	parsedUrl, err := url.Parse(websiteUrl)
+
+	if err == nil {
+		folderName := parsedUrl.Hostname()
+
+		return folderName, nil
+	}
+
+	return "", err
+}
 
 func main() {
 	websiteUrl, exists := os.LookupEnv("WEBSITE_URL")
 	if exists {
 		fmt.Printf("Retrieved WEBSITE_URL=%s\n", websiteUrl)
+
+		goPath := os.Getenv("GOPATH")
+		cmd := exec.Command(goPath+"/bin/goclone", websiteUrl)
+		err := cmd.Run()
+		if err != nil {
+			fmt.Printf("Failed to clone %s\n", websiteUrl)
+		} else {
+			folderName, err := GetFolderName(websiteUrl)
+			if err != nil {
+				fmt.Printf("Failed to clone %s\n", websiteUrl)
+			} else {
+				cmd := exec.Command(goPath+"/bin/goclone", "-s", folderName)
+				err := cmd.Run()
+				if err != nil {
+					fmt.Printf("Failed to serve %s\n", folderName)
+				}
+			}
+		}
 	} else {
 		fmt.Printf("Failed to retrieve WEBSITE_URL from environment!\n")
 	}
